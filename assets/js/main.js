ded73b993cf93f19ca805406ccecbea2c05fa60c
Gustavo Nofoente <gustavonofoente@gmail.com>
2017-06-15 10:38:01 -0300

    Removed transitions to prevent possible bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,5 @@ var menuItems = document.querySelector('.menu__items');
 menuIcon.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-
-  if (menuItems.style.opacity == 0) {
-    menuItems.classList.toggle('menu__items--visible');
-    setTimeout(function(){
-      menuItems.style.opacity = 1;
-    }, 50);
-  } else {
-    menuItems.style.opacity = 0;
-    setTimeout(function(){
-      menuItems.classList.toggle('menu__items--visible');
-    }, 150);
-  }
+  menuItems.classList.toggle('menu__items--visible');
 }
