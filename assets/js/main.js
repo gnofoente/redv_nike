1b8dcb595c6e724ce10f74e720cbb57e5b1964c0
Gustavo Nofoente <gustavonofoente@gmail.com>
2017-06-15 10:33:07 -0300

    Added some transition to the menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,16 @@ var menuItems = document.querySelector('.menu__items');
 menuIcon.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  menuItems.classList.toggle('menu__items--visible');
+
+  if (menuItems.style.opacity == 0) {
+    menuItems.classList.toggle('menu__items--visible');
+    setTimeout(function(){
+      menuItems.style.opacity = 1;
+    }, 50);
+  } else {
+    menuItems.style.opacity = 0;
+    setTimeout(function(){
+      menuItems.classList.toggle('menu__items--visible');
+    }, 150);
+  }
 }
