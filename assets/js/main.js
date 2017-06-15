d183cac5b80e305070589f8039be6ccbaff1055a
Gustavo Nofoente <gustavonofoente@gmail.com>
2017-06-15 12:44:48 -0300

    Improved class toggling functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-var menuIcon = document.querySelector('.menu__icon');
-var menuItems = document.querySelector('.menu__items');
-menuIcon.addEventListener('click', toggleMenu);
+document.addEventListener('DOMContentLoaded', function() {
 
-function toggleMenu() {
-  menuItems.classList.toggle('menu__items--visible');
-}
+  var menuIcon = document.querySelector('.menu__icon');
+  var menuItems = document.querySelector('.menu__items');
+  var filter = document.querySelector('.filter-options');
+  var filterButton = document.querySelectorAll('.button--filter');
+
+  menuIcon.addEventListener('click', function() {
+    toggleClass(menuItems, 'menu__items--visible');
+  });
+
+  filterButton.forEach(function(val) {
+    val.addEventListener('click', function() {
+      toggleClass(filter, 'filter-options--visible');
+    });
+  })
+
+  function toggleClass(element, elementClass) {
+    element.classList.toggle(elementClass);
+  }
+  
+});
